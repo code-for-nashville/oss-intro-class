8e32dca6234bdcb3ccb3a417951c61479514e31d
Adam Barnhard <adam.barnhard@digitalreasoning.com>
2017-02-25 16:08:14 -0600

    Issue-16 updated subtraction test

--- node/test/test_calculator.js
+++ node/test/test_calculator.js
@@ -6,10 +6,13 @@ const calculator = require('../index.js')
 
 ava.test('add', t => {
   t.is(2, calculator.add(1, 1));
-})
+});
 
 ava.test('add_#2', t => {
   t.is(7, calculator.add(5, 2));
 })
 
-ava.test('subtract', t => t.pass())
+ava.test('subtract', t => {
+  t.is(0, calculator.subtract(1, 1));
+});
+
