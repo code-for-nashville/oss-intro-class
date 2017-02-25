94cb9e86cc03eb30f9618ef96daf7488a923522e
David Cramb <david.cramb@gmail.com>
2017-02-25 16:17:32 -0600

    Fixed issue where addition command always returned "2" Added unit test for addition with 3 test cases

--- python/test/test_calculator.py
+++ python/test/test_calculator.py
@@ -2,7 +2,10 @@ import calculator
 
 
 def test_add():
+    assert calculator.add(3, 9) == 12
     assert calculator.add(1, 1) == 2
+    assert calculator.add(10, 5) == 15
+
 
 def test_multiply():
     assert calculator.multiply(2, 2) == 4
